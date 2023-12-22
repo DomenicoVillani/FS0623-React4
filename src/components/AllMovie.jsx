cc36c8d83bf438d3438cc2576e4853a3367162bc
Domenico Villani <domenico.villani.99@gmail.com>
2023-12-22 17:26:11 +0100

    migliorie ma neanche cosi tante

--- src/components/AllMovie.jsx
+++ src/components/AllMovie.jsx
@@ -6,8 +6,8 @@ const AllMovie = () =>{
         <Container>
             <Row>
                 <Col xs={12}>
-                    {/* <MyListFilm listFilm='http://www.omdbapi.com/?apikey=8288927f&s=avengers' title='Avengers'/>
-                    <MyListFilm listFilm='http://www.omdbapi.com/?apikey=8288927f&s=star%20wars' title='Star Wars'/> */}
+                    <MyListFilm listFilm='http://www.omdbapi.com/?apikey=8288927f&s=avengers' title='Avengers'/>
+                    <MyListFilm listFilm='http://www.omdbapi.com/?apikey=8288927f&s=star%20wars' title='Star Wars'/>
                     <MyListFilm listFilm='http://www.omdbapi.com/?apikey=8288927f&s=harry%20potter' title='Harry Potter'/>
                 </Col>
             </Row>
