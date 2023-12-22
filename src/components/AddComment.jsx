493cc7e495a99730f2058af832b27d3f1c60f55b
Domenico Villani <domenico.villani.99@gmail.com>
2023-12-22 18:06:04 +0100

    migliorie ma neanche cosi tante

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -19,7 +19,7 @@ class AddComment extends React.Component {
                 headers: {
                     Authorization:
                         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTg0M2Y3NmI1MjViYjAwMThlZDA3ZWIiLCJpYXQiOjE3MDMxNjU4MTUsImV4cCI6MTcwNDM3NTQxNX0.mKuoXW7tenNwNxL9AV5BAsLcICUV0zAH2fIZgNokT0Q',
-                    'Content-Type': 'apllication/json',
+                    'Content-Type': 'application/json',
                 }
             }
             )
